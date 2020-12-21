702194297f99f4d436a76ed90477a240ff27498e
mamba2410 <callummcgregor54@gmail.com>
2020-12-21 15:24:55 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+
+use std::process;
+
+fn main() {
+
+    // get name
+    // verify with regex
+    //
+    // Default flags
+    // Argument loop
+    // 
+    // Set home for languages etc
+    //
+    // Check if language is valid
+    // Check if license is valid
+    //
+    // Check if directory can be made
+    //
+    // Create dir and cd to it
+    // Create tree
+    // Copy docs
+    // Change docs
+    // Call language bash script
+    // 
+    // check and init git
+    // check and set remote
+
+}
+
+
+fn exit_usage() {
+    println!("
+    Usage:
+    ");
+
+    process::exit(-1);
+}
+
